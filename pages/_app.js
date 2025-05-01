f985182ef2df82afc3dcb634be2be84f4d5c83d9
Wesd.eth <87329637+wesdeth@users.noreply.github.com>
2025-05-01 17:48:50 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-
 import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster position="top-right" />
+    </>
+  );
 }
