5d1017d3a23dde111d96055ac78e7a4425f841e4
Wesd.eth <87329637+wesdeth@users.noreply.github.com>
2025-05-01 16:32:28 -0400

    Update [ens].js

--- pages/[ens].js
+++ pages/[ens].js
@@ -1,9 +1,13 @@
-
 import ENSProfile from '../components/ENSProfile';
 import { useRouter } from 'next/router';
 
 export default function ENSPage() {
   const router = useRouter();
   const { ens } = router.query;
+
+  if (!ens) {
+    return <p className="text-center mt-10">Loading ENS profile...</p>;
+  }
+
   return <ENSProfile ensName={ens} />;
 }
