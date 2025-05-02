f6ebd5014a1d56e045abc5dc4fd5adf66a5dffa1
Wesd.eth <87329637+wesdeth@users.noreply.github.com>
2025-05-02 13:58:08 -0400

    Add files via upload

---
+++ pages/preview/[ensName].js
@@ -0,0 +1,10 @@
+
+// [ensName].js
+export default function Preview() {
+  return (
+    <div style={{ padding: "3rem", fontFamily: "sans-serif" }}>
+      <h1>This is the Preview Resume page.</h1>
+      <p>This is where the profile will be rendered for {'{ensName}'}</p>
+    </div>
+  );
+}
