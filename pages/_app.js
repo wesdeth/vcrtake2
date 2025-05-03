8fb86f7744ef9cfda8886ee67428887bac271bb8
Wesd.eth <87329637+wesdeth@users.noreply.github.com>
2025-05-03 02:32:44 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
+import { WagmiConfig, createConfig, configureChains } from 'wagmi';
+import { mainnet } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const { chains, publicClient } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  publicClient,
+});
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Component {...pageProps} />
-      <Toaster position="top-right" />
-    </>
+    <WagmiConfig config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <Toaster position="top-right" />
+      </QueryClientProvider>
+    </WagmiConfig>
   );
 }
