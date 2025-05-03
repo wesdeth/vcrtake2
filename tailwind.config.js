e108f75b6acf52198b7bc5721ac06c3ce15ecc5e
Wesd.eth <87329637+wesdeth@users.noreply.github.com>
2025-05-03 02:40:00 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // <- this is the key line
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
