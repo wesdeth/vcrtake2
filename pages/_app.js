ec98c6e8761f166befe759ae1676044bbad61312
Wesd.eth <87329637+wesdeth@users.noreply.github.com>
2025-05-04 13:32:44 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,22 @@
 // pages/_app.js
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
-import { WagmiConfig, createConfig, configureChains } from 'wagmi';
+import { WagmiConfig, configureChains, createConfig } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { publicProvider } from 'wagmi/providers/public';
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
-import { Web3Modal } from '@web3modal/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Chains & Providers
-const chains = [mainnet];
-const projectId = 'YOUR_PROJECT_ID_HERE'; // Get this from https://cloud.walletconnect.com/
+const { chains, publicClient } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
 
 const wagmiConfig = createConfig({
   autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
-  publicClient: configureChains(chains, [w3mProvider({ projectId }), publicProvider()]).publicClient
+  connectors: [],
+  publicClient,
 });
 
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
@@ -27,7 +25,6 @@ export default function App({ Component, pageProps }) {
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
         <Toaster position="top-right" />
-        <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
       </QueryClientProvider>
     </WagmiConfig>
   );
