6d0b4aecb742e774bdf3d6437b901732cad3eb9e
Wesd.eth <87329637+wesdeth@users.noreply.github.com>
2025-05-04 12:42:49 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,35 @@
+// pages/_app.js
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
-import { WagmiConfig, createConfig, configureChains } from 'wagmi';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  WagmiConfig,
+  createConfig,
+  configureChains
+} from 'wagmi';
 import { mainnet } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+// RainbowKit chain and wallet configuration
 const { chains, publicClient } = configureChains(
   [mainnet],
   [publicProvider()]
 );
 
+const { connectors } = getDefaultWallets({
+  appName: 'Lookup.xyz',
+  projectId: 'YOUR_PROJECT_ID_HERE', // Optional but recommended (WalletConnect Cloud)
+  chains
+});
+
 const wagmiConfig = createConfig({
   autoConnect: true,
-  publicClient,
+  connectors,
+  publicClient
 });
 
 const queryClient = new QueryClient();
@@ -21,8 +38,10 @@ export default function App({ Component, pageProps }) {
   return (
     <WagmiConfig config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <Toaster position="top-right" />
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+          <Toaster position="top-right" />
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiConfig>
   );
