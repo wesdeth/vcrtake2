356a1e7de99b9bba30c1b000f59287d1382fb890
Wesd.eth <87329637+wesdeth@users.noreply.github.com>
2025-05-06 12:12:53 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import { mainnet } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Head from 'next/head';
 
 const { chains, publicClient } = configureChains(
   [mainnet],
@@ -28,6 +29,12 @@ export default function App({ Component, pageProps }) {
   return (
     <WagmiConfig config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cal+Sans:wght@600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <Component {...pageProps} />
         <Toaster position="top-right" />
       </QueryClientProvider>
