8694852ad78b8e8ea335bcb0b4461f3cdf2586c0
Wesd.eth <87329637+wesdeth@users.noreply.github.com>
2025-05-06 12:18:15 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,18 +8,11 @@ import { InjectedConnector } from 'wagmi/connectors/injected';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Head from 'next/head';
 
-const { chains, publicClient } = configureChains(
-  [mainnet],
-  [publicProvider()]
-);
+const { chains, publicClient } = configureChains([mainnet], [publicProvider()]);
 
 const wagmiConfig = createConfig({
   autoConnect: true,
-  connectors: [
-    new InjectedConnector({
-      chains,
-    }),
-  ],
+  connectors: [new InjectedConnector({ chains })],
   publicClient,
 });
 
@@ -27,17 +20,19 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Cal+Sans:wght@600&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <Component {...pageProps} />
-        <Toaster position="top-right" />
-      </QueryClientProvider>
-    </WagmiConfig>
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cal+Sans:wght@600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <WagmiConfig config={wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          <Toaster position="top-right" />
+        </QueryClientProvider>
+      </WagmiConfig>
+    </>
   );
 }
